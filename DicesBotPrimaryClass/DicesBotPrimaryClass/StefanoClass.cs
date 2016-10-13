36563fb8fbd5f3dd0443a3e86d797b59be1b3865
caneslotocato <caneslotocato1@gmail.com>
2016-10-13 23:33:31 +0200

    Adding StefanoClass

---
+++ DicesBotPrimaryClass/DicesBotPrimaryClass/StefanoClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Commands;
+using Discord;
+using Discord.API;
+using Discord.Audio;
+using Discord.ETF;
+using Discord.Legacy;
+using Discord.Logging;
+using Discord.Modules;
+using Discord.Net;
+
+namespace DicesBotPrimaryClass
+{
+    class StefanoClass
+    {
+
+    }
+}
