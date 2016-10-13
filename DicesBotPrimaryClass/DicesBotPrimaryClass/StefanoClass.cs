fd39e8ad5a7aeb4ac0cf45938fcd7d355a61c71d
caneslotocato <caneslotocato1@gmail.com>
2016-10-13 23:51:41 +0200

    LE COSE DIFFICILI

--- DicesBotPrimaryClass/DicesBotPrimaryClass/StefanoClass.cs
+++ DicesBotPrimaryClass/DicesBotPrimaryClass/StefanoClass.cs
@@ -17,6 +17,25 @@ namespace DicesBotPrimaryClass
 {
     class StefanoClass
     {
+        private int randomNumber = 0;
+        public string theNumber;
+        
+
+        public int ToCall(int dado, int nDadi)
+        {
+            for(int i = 0; i < nDadi; i++)
+            {
+                randomNumber = Random(dado);
+                theNumber = Convert.ToString(randomNumber);
+                Console.WriteLine(theNumber);
+            }
+        }
+
+        public int Random(int dado)
+        {
+            Random value = new Random();
+            int rndValue = value.Next(1, dado + 1);
+        }
 
     }
 }
