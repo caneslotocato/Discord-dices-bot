85faccd72b536528d9fdb0e62ccf14d3b0e7b711
Gallettas <giaky041998@gmail.com>
2016-10-14 00:20:47 +0200

    add main metod with token

--- DicesBotPrimaryClass/DicesBotPrimaryClass/Program.cs
+++ DicesBotPrimaryClass/DicesBotPrimaryClass/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿using Discord;
+
+class Program {
+    static void Main(string[] args) => new Program().Start();
+
+    private DiscordClient _client;
+
+    public void Start() {
+        _client = new DiscordClient();
+
+        _client.MessageReceived += async (s, e) =>
+        {
+            if (!e.Message.IsAuthor)
+                await e.Channel.SendMessage(e.Message.Text);
+        };
+
+        _client.ExecuteAndWait(async () => {
+            await _client.Connect("A2DIIL47bpuwEA2_TQ6K2yGOewubZKQE", TokenType.Bot)
+        });
+    }
+}
